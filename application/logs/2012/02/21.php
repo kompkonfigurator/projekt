9abf3084c860350ccdb9f24496153b8d301e04f9
Michal Mroczkowski <misiom1@gmail.com>
2012-02-21 22:44:05 +0100

    x

--- application/logs/2012/02/21.php
+++ application/logs/2012/02/21.php
@@ -358,3 +358,10 @@
 #9 C:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
 #10 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
 #11 {main}
+2012-02-21 15:36:58 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL article was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-02-21 15:36:58 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL article was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 C:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 C:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#3 {main}
