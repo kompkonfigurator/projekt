6f3a687270c9c6781f867496ab832cc1e4e3bbc5
pcirocki <p.cirocki@gmail.com>
2012-03-14 12:28:11 +0100

    edycja formularza logowania

--- application/views/login.php
+++ application/views/login.php
@@ -1,14 +1,16 @@
-<h1><?php echo $title?></h1>
+<h1><?php echo "Logowanie : "?></h1>
+<br/>
+<br/>
 <?php
 echo  Form::open('/auth/login').
     '<fieldset>'.
-        '<legend>'.__('Fill the fields').'</legend>'.
+        '<legend>'.__('Pole logowania').'</legend>'.
         $error.
-        Form::label('nick', __('Nick').':').
+        Form::label('nick', __('Login').':').'<br/>'.
         Form::input('nick', $data['nick']).'<br />'.
  
-        Form::label('pass', __('Password').':').
+        Form::label('pass', __('Hasło').':').'<br/>'.
         Form::password('pass', $data['pass']).'<br />'.
-    '</fieldset>'.
-    Form::submit('submit', __('Login')).
+    '</fieldset>'.'<br/>'.'<br/>'.
+    Form::submit('submit', __('Zaloguj')).
 Form::close();?>
