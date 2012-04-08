d43b18b151d0f8c66f94c5764ac7bccce8bc59c8
Misio <misiom1@gmail.com>
2012-04-08 19:00:39 +0200

    fixed form

--- application/classes/controller/form.php
+++ application/classes/controller/form.php
@@ -4,18 +4,18 @@ class Controller_Form extends Controller_Default {
      public function action_index() {
 		$form = Model::factory('form');
         $this->template->title = __('Formularz');
-        $this->template->content=view::factory('form');//view::factory?
-		$this->template->plyta = $form->get_produkt('plyta');
-		$this->template->procesor = $form->get_produkt('procesor');
-		$this->template->pamiec = $form->get_produkt('pamiec');
-		$this->template->karta_graf = $form->get_produkt('karta_graf');
-		$this->template->dysk = $form->get_produkt('dysk');
-		$this->template->obudowa = $form->get_produkt('obudowa');
-		$this->template->zasilacz = $form->get_produkt('zasilacz');
-		$this->template->naped = $form->get_produkt('naped');
-		$this->template->karta_muz = $form->get_produkt('karta_muz');
-		$this->template->mysz = $form->get_produkt('mysz');
-		$this->template->klawiatura = $form->get_produkt('klawiatura');
+        $this->template->content = View::factory('form');
+		$this->template->content->plyta = $form->get_produkt('plyta');
+		$this->template->content->procesor = $form->get_produkt('procesor');
+		$this->template->content->pamiec = $form->get_produkt('pamiec');
+		$this->template->content->karta_graf = $form->get_produkt('karta_graf');
+		$this->template->content->dysk = $form->get_produkt('dysk');
+		$this->template->content->obudowa = $form->get_produkt('obudowa');
+		$this->template->content->zasilacz = $form->get_produkt('zasilacz');
+		$this->template->content->naped = $form->get_produkt('naped');
+		$this->template->content->karta_muz = $form->get_produkt('karta_muz');
+		$this->template->content->mysz = $form->get_produkt('mysz');
+		$this->template->content->klawiatura = $form->get_produkt('klawiatura');
     }
  }
 ?>
---
+++ application/logs/2012/04/08.php
@@ -0,0 +1,78 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-04-08 11:52:08 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2012-04-08 11:52:08 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-04-08 11:52:13 --- ERROR: ErrorException [ 8 ]: Undefined variable: plyta ~ APPPATH\views\form.php [ 12 ]
+2012-04-08 11:52:13 --- STRACE: ErrorException [ 8 ]: Undefined variable: plyta ~ APPPATH\views\form.php [ 12 ]
+--
+#0 C:\xampp\htdocs\kohana\application\views\form.php(12): Kohana_Core::error_handler(8, 'Undefined varia...', 'C:\xampp\htdocs...', 12, Array)
+#1 C:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('C:\xampp\htdocs...')
+#2 C:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('C:\xampp\htdocs...', Array)
+#3 C:\xampp\htdocs\kohana\system\classes\kohana\view.php(228): Kohana_View->render()
+#4 C:\xampp\htdocs\kohana\application\views\default.php(69): Kohana_View->__toString()
+#5 C:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('C:\xampp\htdocs...')
+#6 C:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('C:\xampp\htdocs...', Array)
+#7 C:\xampp\htdocs\kohana\system\classes\kohana\controller\template.php(44): Kohana_View->render()
+#8 C:\xampp\htdocs\kohana\application\classes\controller\default.php(69): Kohana_Controller_Template->after()
+#9 [internal function]: Controller_Default->after()
+#10 C:\xampp\htdocs\kohana\system\classes\kohana\request\client\internal.php(121): ReflectionMethod->invoke(Object(Controller_Form))
+#11 C:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#12 C:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#13 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#14 {main}
+2012-04-08 11:52:13 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2012-04-08 11:52:13 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-04-08 11:52:30 --- ERROR: ErrorException [ 8 ]: Undefined variable: plyta ~ APPPATH\views\form.php [ 12 ]
+2012-04-08 11:52:30 --- STRACE: ErrorException [ 8 ]: Undefined variable: plyta ~ APPPATH\views\form.php [ 12 ]
+--
+#0 C:\xampp\htdocs\kohana\application\views\form.php(12): Kohana_Core::error_handler(8, 'Undefined varia...', 'C:\xampp\htdocs...', 12, Array)
+#1 C:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('C:\xampp\htdocs...')
+#2 C:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('C:\xampp\htdocs...', Array)
+#3 C:\xampp\htdocs\kohana\system\classes\kohana\view.php(228): Kohana_View->render()
+#4 C:\xampp\htdocs\kohana\application\views\default.php(69): Kohana_View->__toString()
+#5 C:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('C:\xampp\htdocs...')
+#6 C:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('C:\xampp\htdocs...', Array)
+#7 C:\xampp\htdocs\kohana\system\classes\kohana\controller\template.php(44): Kohana_View->render()
+#8 C:\xampp\htdocs\kohana\application\classes\controller\default.php(69): Kohana_Controller_Template->after()
+#9 [internal function]: Controller_Default->after()
+#10 C:\xampp\htdocs\kohana\system\classes\kohana\request\client\internal.php(121): ReflectionMethod->invoke(Object(Controller_Form))
+#11 C:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#12 C:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#13 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#14 {main}
+2012-04-08 11:52:30 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2012-04-08 11:52:30 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-04-08 11:57:18 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2012-04-08 11:57:18 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-04-08 11:57:29 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2012-04-08 11:57:29 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-04-08 11:58:01 --- ERROR: ErrorException [ 2048 ]: Creating default object from empty value ~ APPPATH\classes\controller\form.php [ 6 ]
+2012-04-08 11:58:01 --- STRACE: ErrorException [ 2048 ]: Creating default object from empty value ~ APPPATH\classes\controller\form.php [ 6 ]
+--
+#0 C:\xampp\htdocs\kohana\application\classes\controller\form.php(6): Kohana_Core::error_handler(2048, 'Creating defaul...', 'C:\xampp\htdocs...', 6, Array)
+#1 [internal function]: Controller_Form->action_index()
+#2 C:\xampp\htdocs\kohana\system\classes\kohana\request\client\internal.php(118): ReflectionMethod->invoke(Object(Controller_Form))
+#3 C:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#4 C:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#5 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#6 {main}
+2012-04-08 11:58:18 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2012-04-08 11:58:18 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/js/jquery-1.5.1.min.js ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#1 {main}
