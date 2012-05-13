4584e9bc1d00ff4898d0c43124bac8f1c253d444
Aneta B <aneta57@o2.pl>
2012-05-13 21:35:30 +0200

    rejestracja css

--- application/views/user/create.php
+++ application/views/user/create.php
@@ -1,37 +1,48 @@
-<h2>Create a New User</h2>
+<div class="pudelko"><center><h3>Rejestracja</h3>
 <? if ($message) : ?>
 	<h3 class="message">
 		<?= $message; ?>
 	</h3>
 <? endif; ?>
-
+<br/>
+<br/>
 <?= Form::open('user/create'); ?>
 
-<?= Form::label('username', 'Username'); ?>
-<?= Form::input('username', HTML::chars(Arr::get($_POST, 'username'))); ?>
+<div class="rejestracja"><?= Form::label('username', 'Podaj nazwę użytkownika : '); ?></div> <br/>
+<div class="inputy"><?= Form::input('username', HTML::chars(Arr::get($_POST, 'username'))); ?></div>
 <div class="error">
 	<?= Arr::get($errors, 'username'); ?>
 </div>
-
-<?= Form::label('email', 'Email Address'); ?>
+<br/>
+<br/>
+<div class="rejestracja"><?= Form::label('email', 'Podaj adress email : '); ?></div> <br/>
 <?= Form::input('email', HTML::chars(Arr::get($_POST, 'email'))); ?>
 <div class="error">
 	<?= Arr::get($errors, 'email'); ?>
 </div>
+<br/>
+<br/>
 
-<?= Form::label('password', 'Password'); ?>
+<div class="rejestracja"><?= Form::label('password', 'Podaj hasło : '); ?></div> <br/>
 <?= Form::password('password'); ?>
 <div class="error">
 	<?= Arr::path($errors, '_external.password'); ?>
 </div>
+<br/>
+<br/>
 
-<?= Form::label('password_confirm', 'Confirm Password'); ?>
+<div class="rejestracja"><?= Form::label('password_confirm', 'Powtórz hasło : '); ?></div> <br/>
 <?= Form::password('password_confirm'); ?>
 <div class="error">
 	<?= Arr::path($errors, '_external.password_confirm'); ?>
 </div>
+<br/>
+<br/>
+<br/>
+<br/>
 
-<?= Form::submit('create', 'Create User'); ?>
+<?= Form::submit('create', 'Zarejestruj'); ?>
 <?= Form::close(); ?>
-
-<p>Or <?= HTML::anchor('user/login', 'login'); ?> if you have an account already.</p>
+</center>
+<p style="text-align:center">Or <?= HTML::anchor('user/login', 'login'); ?> if you have an account already.</p>
+</div>
